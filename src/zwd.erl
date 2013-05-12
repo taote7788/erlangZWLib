95438d99443ac4fe1a11c8f8919bd3fcfbd0f000
taote7788 <taote7788@163.com>
2013-05-12 20:13:27 +0800

    add is_string and is_printChar

--- src/zwd.erl
+++ src/zwd.erl
@@ -8,9 +8,44 @@
 %%%-----------------------------------------------
 
 -module(zwd).
--export([print/1,p/1]).
+-compile(export_all).
+
+
+
+%%-----------------------------------------------
+%% @doc 本方法打印出uncode 的字符
+%% @spec 
+%% ->{Value} 
+%% @end 
+%%-----------------------------------------------
 
 print(ListData)->
     io:format("~ts~n", [list_to_binary(ListData)]).
 p(ListData)->
     print(ListData).
+
+
+
+%%-----------------------------------------------
+%% @doc 看这个是否为一个可打印的字符
+%% @spec 
+%% 
+%% @end 
+%%-----------------------------------------------
+is_printChar(XX)->
+    if XX < 0 -> false;  
+       XX > 255 -> false;
+       true -> true      
+    end.
+
+%%-----------------------------------------------
+%% @doc 看字符串是否为全部可打印的字符串
+%% @spec 
+%% 
+%% @end 
+%%-----------------------------------------------
+is_string(XY) ->            
+    case is_list(XY) of           
+        false -> false;           
+        true -> lists:all(is_printChar,XY)
+    end.
